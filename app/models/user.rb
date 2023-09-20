fd1ceff864e18b9a927b6a4dd2b22153bab09b4d
Enoisong <enobongitiaba988@gmail.com>
2023-09-19 20:29:48 -0700

    Style Splash

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
-  has_many :transactions
-  has_many :groups
-end
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable, :trackable, and :omniauthable
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :validatable
+  
+    has_many :transactions
+    has_many :categories
+    has_many :groups
+  
+    validates :full_name, presence: true
+    validates :email, presence: true, uniqueness: true
+  end
+  
