c0bfcd6de80da0e2af6284213995c97825ba61ee
Enoisong <enobongitiaba988@gmail.com>
2023-09-23 04:25:22 -0700

    clean up code

--- spec/features/categories_spec.rb
+++ spec/features/categories_spec.rb
@@ -1,29 +1,23 @@
-# spec/features/categories_spec.rb
 require 'rails_helper'
 
-RSpec.feature 'Categories', type: :feature do
-  it 'User visits new category page' do
-    # visit new_category_path
-    # expect(page).to have_content("Create a New Category")
+RSpec.describe CategoriesController, type: :controller do
+  before :each do
+    @user = User.create!(full_name: 'John Doe', email: 'john@example.com', password: 'password',
+                         password_confirmation: 'password')
+    sign_in @user
   end
 
-  it 'User fills out category creation form correctly' do
-    # visit new_category_path
-    # fill_in "Name", with: "Groceries"
-    # fill_in "Icon", with: "fa-shopping-cart"
-    # click_button "Save"
-    # expect(page).to have_content("Groceries")
+  describe 'GET #index' do
+    it 'returns http success' do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
   end
 
-  it 'User visits categories index page' do
-    # visit categories_path
-    # expect(page).to have_content("Categories")
-  end
-
-  it "User clicks on 'View Transactions' link for a category" do
-    # category = Category.create(name: "Groceries")
-    # visit categories_path
-    # click_link "View Transactions", href: category_transactions_path(category)
-    # expect(page).to have_content("Transactions for Groceries")
+  describe 'GET #new' do
+    it 'returns http success' do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
   end
 end
--- spec/features/splash_spec.rb
+++ spec/features/splash_spec.rb
@@ -1,15 +1,8 @@
-# spec/features/splash_spec.rb
 require 'rails_helper'
 
-RSpec.feature 'Splash', type: :feature do
-  it 'User visits the splash screen' do
-    visit root_path
-    expect(page).to have_content('Welcome to My Budget App')
-  end
-
-  it "User clicks 'Sign Up' link on the splash screen" do
-    # visit root_path
-    # click_link "Sign Up", href: new_user_registration_path
-    # expect(page).to have_content("Sign up")
+RSpec.feature "Splash", type: :feature do
+  scenario "should display the Splash by redirecting" do
+    visit root_path  
+    expect(page).to have_content("My Budget App")  
   end
 end
